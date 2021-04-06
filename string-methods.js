49e4ff4e98e760dc1d84df19f525499f4da0b4b5
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-04-06 17:32:11 +0200

    Add solutions for string-methods.js

--- string-methods.js
+++ string-methods.js
@@ -13,7 +13,14 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
+console.log(fruit.length > vegetable.length);
 
+// dit mag ook:
+// if (fruit.length > vegetable.length) {
+//  console.log(true);
+// } else {
+//  console.log(false);
+// }
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,7 +28,13 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
+const lastIndexFruit = fruit.length - 1;
+const lastIndexVegetable = vegetable.length - 1;
 
+const lastFruit = fruit.substring(lastIndexFruit);
+const lastVegetable = vegetable.substring(lastIndexVegetable);
+
+console.log(lastFruit, lastVegetable);
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
@@ -29,7 +42,8 @@ const vegetable = "Aardappel";
 
 const userInput = "  De kat krabt de krullen van de trap ";
 
-
+const trimmed = userInput.trim();
+console.log(trimmed);
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
@@ -37,13 +51,17 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
-
+const hasWord = story.includes("Fantastisch");
+console.log(hasWord);
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
+const endOfSentence = story.indexOf('.');
 
+const sentence = story.substring(0, endOfSentence + 1);
+console.log(sentence);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,6 +71,9 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
+
+const euroPrice = price.replace("$", "€").replace(".", ",");
+console.log(euroPrice);
 
 
 
